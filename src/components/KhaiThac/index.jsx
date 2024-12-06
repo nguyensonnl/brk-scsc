8963eea806881c72d7bcf735d8e3dbfdfd03b4f4
nguyensonnl <nguyenquynhson30446@gmail.com>
2024-12-06 22:51:12 +0700

    update

--- src/components/KhaiThac/index.jsx
+++ src/components/KhaiThac/index.jsx
@@ -1,5 +1,16 @@
 function KhaiThac() {
-  return <div className="main">Khai Thác</div>;
+  return <div className="main">
+  <h3>Các vấn đề khai thác</h3>
+  <ul>
+	<li>
+	Lô hàng có 2 hawb, hawb A trên hệ thống có 2 kiện, hawb B có 1 kiện, thực tế hawb A có 3 kiện, hawb B không có kiện nào (shiping mark của hawb B) </br>
+	- Nhận 1 pc hawb B vào hawb A <br/>
+	- Hawb A mark: thực tế nhận 3 pcs, nhận tạm 1 pc vào hawb B </br>
+	- Hawb B mark: sai hawb, hawb trên hàng -> Làm sai hawb bình thường
+	</li>
+  </ul>
+
+ </div>
 }
 
 export default KhaiThac;
