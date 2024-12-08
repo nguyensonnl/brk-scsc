dc9dd4a26bb908c263cdc51cc0d1d432aeb83080
nguyensonnl <nguyenquynhson30446@gmail.com>
2024-12-08 21:26:44 +0700

    update

--- src/components/KhaiThacHangLanh/index.jsx
+++ src/components/KhaiThacHangLanh/index.jsx
@@ -1,10 +1,15 @@
 const KhaiThacHangLanh = () => {
-  return <div className="main">
-  <h3>Khai Thác Hàng Lạnh</h3>
-  <ol>
-	<li>Dry Ice tài liệu mark: code col, dry ice, brk xem nhiệt độ trện hàng: nhiệt độ trên hàng freeze upon arrival: move frz</li>
-  </ol>
-  </div>;
+  return (
+    <div className="main">
+      <h3>Khai Thác Hàng Lạnh</h3>
+      <ol>
+        <li>
+          Dry Ice tài liệu mark: code col, dry ice, brk xem nhiệt độ trện hàng:
+          nhiệt độ trên hàng freeze upon arrival: move frz
+        </li>
+      </ol>
+    </div>
+  );
 };
 
 export default KhaiThacHangLanh;
