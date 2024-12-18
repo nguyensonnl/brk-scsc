dbbb1146756e0d89b75fdefb8b0f277b1d8a2d17
Nguyễn Sơn <107752874+nguyensonnl@users.noreply.github.com>
2024-12-18 16:34:57 +0700

    Update index.jsx

--- src/components/ThongBao/index.jsx
+++ src/components/ThongBao/index.jsx
@@ -3,13 +3,14 @@ const ThongBao = () => {
     <div className="main">
       <h3>Thông báo</h3>
       <ol>
+        <li>No FHL, dư FHL: Nếu trên hàng có HAWB thì nhận theo HAWB, không HAWB thì nhận theo MAWB (vào location add group).</li>
         <li>
           Lô hàng Transit VJ, nếu không có mail không xả, thì nhắn Zalo cho
-          group VJ, rồi báo gửi mail gấp để khai thác
+          group VJ, rồi báo gửi mail gấp để khai thác.
         </li>
         <li>
-          Lô hàng cần thời gian move, sau khi move lạnh, remark đã move cho
-          tracing tính phí
+          Lô hàng yêu cầu chuyển lạnh có thời gian cụ thể, sau khi move lạnh, remark đã move cho
+          tracing tính phí, vd: lô hàng về ngày 10/12 move 15 - 25, nhưng yêu cầu 12/12 move 2 - 8.
         </li>
       </ol>
     </div>
